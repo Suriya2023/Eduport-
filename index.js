91089eed6fe4dc06d30cb8a49950ba0dfe191e49
Suriya Singh Rajput <148303377+Suriya2023@users.noreply.github.com>
2024-05-08 14:09:02 +0530

    PortFolio_Demo_Project

---
+++ index.js
@@ -0,0 +1,99 @@
+{/* <script> */}
+                function MyonHindddenshow(){
+                    // var x = document.getElementById("hidenshow");
+                    // if (x.style.display="block") {
+                    //             x.style.display="none"
+                    // } else {
+                    //     x.style.display="block"
+                        
+                    // }
+                    if (document.getElementById("mysectionSc").style.display =="block") {
+                    document.getElementById("mysectionSc").style.display="none"
+                        
+                    } else {
+                    document.getElementById("mysectionSc").style.display="block"
+                        
+                    }
+                }
+
+                function MyonHindddenshowWEb(){
+                    // var x = document.getElementById("hidenshow");
+                    // if (x.style.display="block") {
+                    //             x.style.display="none"
+                    // } else {
+                    //     x.style.display="block"
+                        
+                    // }
+                    if (document.getElementById("my-sectionsecond-card").style.display =="block") {
+                    document.getElementById("my-sectionsecond-card").style.display="none"
+                        
+                    } else {
+                    document.getElementById("my-sectionsecond-card").style.display="block"
+                        
+                    }
+                }
+
+
+                function MyonHindddenshowWEbtwo(){
+                    // var x = document.getElementById("hidenshow");
+                    // if (x.style.display="block") {
+                    //             x.style.display="none"
+                    // } else {
+                    //     x.style.display="block"
+                        
+                    // }
+                    if (document.getElementById("mysectionSctwo").style.display =="block") {
+                    document.getElementById("mysectionSctwo").style.display="none"
+                        
+                    } else {
+                    document.getElementById("mysectionSctwo").style.display="block"
+                        
+                    }
+                }
+
+
+                function MyonHindddenshowWEbthird(){
+                    // var x = document.getElementById("hidenshow");
+                    // if (x.style.display="block") {
+                    //             x.style.display="none"
+                    // } else {
+                    //     x.style.display="block"
+                        
+                    // }
+                    if (document.getElementById("mysectionSctwothird").style.display =="block") {
+                    document.getElementById("mysectionSctwothird").style.display="none"
+                        
+                    } else {
+                    document.getElementById("mysectionSctwothird").style.display="block"
+                        
+                    }
+                }
+
+
+                function MyonHindddenshowWEbthirdforth(){
+                    // var x = document.getElementById("hidenshow");
+                    // if (x.style.display="block") {
+                    //             x.style.display="none"
+                    // } else {
+                    //     x.style.display="block"
+                        
+                    // }
+                    if (document.getElementById("mysectionSctwothirdForth").style.display =="block") {
+                    document.getElementById("mysectionSctwothirdForth").style.display="none"
+                        
+                    } else {
+                    document.getElementById("mysectionSctwothirdForth").style.display="block"
+                        
+                    }
+                }
+                function ClickMe(){
+                    // var ul = document.getElementById("bom");
+        
+                        if (document.getElementById('bom').style.visibility  == "visible") {
+                            document.getElementById('bom').style.visibility="hidden"
+                        } else {
+                            document.getElementById('bom').style.visibility="visible"
+                        }
+                }
+
+// </script>
